abdaff5541ec68fe9d5467c9e5937314cddceee2
mrhrifat <mrhrifat383759@gmail.com>
2023-02-15 03:38:37 +0600

    refactor: class names

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import LinkCard from "@/components/LinkCard";
 import SocialCard from "@/components/SocialCard";
-import styles from "@/styles";
 import { data, metadata as Metadata } from "@/utils/data";
 import Image from "next/image";
 
@@ -9,21 +8,21 @@ export const metadata = Metadata;
 export default function HomePage() {
   return (
     <>
-      <div className={styles.homepageDiv}>
+      <div className="flex flex-col items-center mx-auto w-full justify-center mt-16 px-8">
         <Image
-          className={styles.homepageImage}
+          className="rounded-full"
           src={data.avatar}
           alt={data.name}
           width={96}
           height={96}
         />
-        <h1 className={styles.homepageH1}>@{data.name}</h1>
+        <h1 className="font-semibold mt-4 mb-8 text-xl">@{data.name}</h1>
 
         {data.links.map((link) => (
           <LinkCard key={link.href} {...link} />
         ))}
 
-        <div className={styles.homepageFooterDiv}>
+        <div className="flex flex-row">
           {data.socials.map((social) => (
             <SocialCard key={social.href} {...social} />
           ))}
