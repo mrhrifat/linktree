decc25ee6be2066fb22e9137d7d8b4926ca26f3a
mrhrifat <mrhrifat383759@gmail.com>
2023-02-15 03:18:44 +0600

    data dir

---
+++ utils/data.ts
@@ -0,0 +1,56 @@
+import { data as Data, metadata as Metadata } from "@/interface";
+
+export const data: Data = {
+  name: "Ibnul Jawji",
+  avatar: "https://avatars.githubusercontent.com/u/45077312?v=4",
+  links: [
+    {
+      title: "Ibnul Jawji Portfolio",
+      href: "https://www.ibnuljawji.com",
+      image: "https://avatars.githubusercontent.com/u/45077312?v=4",
+    },
+    {
+      title: "LinkedIn",
+      href: "https://linkedin.com",
+      image: "https://avatars.githubusercontent.com/u/45077312?v=4",
+    },
+    {
+      title: "Mail",
+      href: "https://mail.com",
+      image: "https://avatars.githubusercontent.com/u/45077312?v=4",
+    },
+    {
+      title: "GitHub",
+      href: "https://github.com",
+      image: "https://avatars.githubusercontent.com/u/45077312?v=4",
+    },
+  ],
+  socials: [
+    {
+      title: "twitter",
+      href: "https://twitter.com",
+      image: "/twitter.svg",
+    },
+    {
+      title: "telegram",
+      href: "https://telegram.com",
+      image: "/telegram.svg",
+    },
+  ],
+};
+
+export const metadata: Metadata = {
+  title: "Linktree",
+  charset: "UTF-8",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  description:
+    "Linktree is an link holder of your different social, bussiness or portfolio link handle",
+  keywords: ["linktree", "portfolio", "social", "media", "social", "media"],
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
